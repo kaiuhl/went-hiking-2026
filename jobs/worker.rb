f8530a5e0c8cf9a480d8dd63c74c8434b0f26ba7
Kyle Meyer <kaiuhl@gmail.com>
2026-05-23 14:16:16 -0700

    Start Que worker with CLI

--- jobs/worker.rb
+++ jobs/worker.rb
@@ -1,7 +1,2 @@
 require_relative "../config/boot"
 require "went_hiking/photo_variant_job"
-
-Que.connection = WentHiking.db
-Que.worker_count = Integer(ENV.fetch("QUE_WORKERS", "2"))
-Que.start
-sleep
